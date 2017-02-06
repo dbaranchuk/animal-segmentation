de64cf91f1d8a347577aa75cc514ff71529b37e7
Dmitry <dmitrybaranchuk@gmail.com>
2017-02-06 04:15:07 +0300

    ...

--- segmentation.py
+++ segmentation.py
@@ -18,7 +18,7 @@ INPUT_SHAPE = (None, 3, 11, 11)
 PAD = 5
 TRAIN_SIZE = 15 #40
 VAL_SIZE = 5 #10
-NUM_EPOCHS = 15
+NUM_EPOCHS = 20
 
 
 # Bottleneck
@@ -58,17 +58,17 @@ class TinyResNet:
         # Input Layer
         l_in = InputLayer(INPUT_SHAPE, input_var=self.input_var)
         # Conv1
-        l_conv1 = Conv2DLayer(l_in, num_filters=32, filter_size=3,
+        l_conv1 = Conv2DLayer(l_in, num_filters=16, filter_size=3,
                             nonlinearity=rectify, W=HeNormal())
         l_conv1 = batch_norm(l_conv1)
         # Conv2
-        l_conv2 = Conv2DLayer(l_conv1, num_filters=64, filter_size=2, stride=2,
+        l_conv2 = Conv2DLayer(l_conv1, num_filters=32, filter_size=2, stride=2,
                             nonlinearity=rectify, W=HeNormal())
         l_conv2 = batch_norm(l_conv2)
         # Residual Block
         #l_res = residual_block(l_conv2, 64)
         l_max = MaxPool2DLayer(l_conv2, pool_size=(2, 2))
-        l_dense = DenseLayer(l_max, num_units=128, nonlinearity=rectify)
+        l_dense = DenseLayer(l_max, num_units=64, nonlinearity=rectify)
         # Softmax Output
         l_out = DenseLayer(l_dense, num_units=2, nonlinearity=softmax)
         self.model = l_out
@@ -114,8 +114,9 @@ class TinyResNet:
     def set_update(self):
         params = get_all_params(self.model, trainable=True)
         self.lr_schedule = {
-            0: 0.001,
-            10: 0.0001
+            5: 0.01,
+            5: 0.001,
+            15: 0.0001
         }
         self.lr = theano.shared(np.float32(self.lr_schedule[0]))
         self.updates = nesterov_momentum(self.train_loss, params,
