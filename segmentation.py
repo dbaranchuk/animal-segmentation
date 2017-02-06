7a721c6ff477d4700bfbbd1a3dd59372447c3a3f
Dmitry <dmitrybaranchuk@gmail.com>
2017-02-06 05:53:17 +0300

    ...

--- segmentation.py
+++ segmentation.py
@@ -14,15 +14,6 @@ from lasagne.nonlinearities import rectify, softmax
 from lasagne.objectives import categorical_crossentropy
 from lasagne.updates import nesterov_momentum
 
-PAD = 6
-TRAIN_SIZE = 25 #40
-VAL_SIZE = 5 #10
-NUM_EPOCHS = 20
-INPUT_SHAPE = (None, 3, 2*PAD + 1, 2*PAD + 1)
-NUM_FILTERS1 = 16
-NUM_FILTERS2 = 16
-NUM_FILTERS3 = 64
-BATCH_SIZE = 9128
 
 # Worker
 MAX_TRAIN_SIZE = 41
@@ -34,36 +25,6 @@ NUM_FILTERS2_SET = (16, 32, 64)
 NUM_FILTERS3_SET = (64, 128, 256)
 
 BATCH_SIZE_SET = (2048, 4096, 9128, 13224, 16256)
-
-def print_params():
-    print ()
-    print ('='*50)
-    print ('PAD = %d' % PAD)
-    print ('TRAIN_SIZE = %d' % TRAIN_SIZE)
-    print ('NUM_FILTERS1 = %d' % NUM_FILTERS1)
-    print ('NUM_FILTERS2 = %d' % NUM_FILTERS2)
-    print ('NUM_FILTERS3 = %d' % NUM_FILTERS3)
-    print ('NUM_EPOCHS = %d' % NUM_EPOCHS)
-    print ('BATCH_SIZE = %d' % BATCH_SIZE)
-    print ('='*50)
-    print ()
-
-
-
-# Bottleneck
-def residual_block(layer, num_filters):
-    l_conv1 = Conv2DLayer(layer, num_filters/4, filter_size=1, pad='same',
-                        nonlinearity=rectify, W=HeNormal())
-    l_conv1 = batch_norm(l_conv1)
-
-    l_conv2 = Conv2DLayer(l_conv1, num_filters/4, filter_size=3, pad='same',
-                        nonlinearity=rectify, W=HeNormal())
-    l_conv2 = batch_norm(l_conv2)
-
-    l_conv3 = Conv2DLayer(l_conv2, num_filters, filter_size=1, pad='same',
-                        nonlinearity=rectify, W=HeNormal())
-    l_conv3 = batch_norm(l_conv3)
-    return ElemwiseSumLayer([l_conv3, layer])
 
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
@@ -80,48 +41,64 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 
 # Net for unary terms
 class TinyNet:
-    # Build Net
-    def __init__(self):
+    # init params
+    def __init__(self, PAD, TRAIN_SIZE, NUM_EPOCHS, NUM_FILTERS1,
+                 NUM_FILTERS2, NUM_FILTERS3, BATCH_SIZE):
+        self.pad = PAD
+        self.train_size = TRAIN_SIZE
+        self.val_size = 5
+        self.num_epochs = NUM_EPOCHS
+        self.num_filters1 = NUM_FILTERS1
+        self.num_filters2 = NUM_FILTERS2
+        self.num_filters3 = NUM_FILTERS3
+        self.batch_size = BATCH_SIZE
+        self.input_shape = (None, 3, 2*PAD + 1, 2*PAD + 1)
         self.input_var = T.tensor4('inputs')
         self.target_var = T.ivector('targets')
+
+    # Build Net
+    def build_cnn(self):
         # Input Layer
-        l_in = InputLayer(INPUT_SHAPE, input_var=self.input_var)
+        l_in = InputLayer(self.input_shape, input_var=self.input_var)
         # Conv1
-        l_conv1 = Conv2DLayer(l_in, num_filters=NUM_FILTERS1, filter_size=3,
-                            nonlinearity=rectify, W=HeNormal())
+        l_conv1 = Conv2DLayer(l_in, num_filters=self.num_filters1, filter_size=3,
+                              nonlinearity=rectify, W=HeNormal())
         l_conv1 = batch_norm(l_conv1)
         # Conv2
-        l_conv2 = Conv2DLayer(l_conv1, num_filters=NUM_FILTERS2, filter_size=2, stride=2,
-                            nonlinearity=rectify, W=HeNormal())
+        l_conv2 = Conv2DLayer(l_conv1, num_filters=self.num_filters2, filter_size=2,
+                              stride=2, nonlinearity=rectify, W=HeNormal())
         l_conv2 = batch_norm(l_conv2)
+        # Pool
         l_max = MaxPool2DLayer(l_conv2, pool_size=(2, 2))
-        l_dense = DenseLayer(l_max, num_units=NUM_FILTERS3, nonlinearity=rectify)
+        # FC
+        l_dense = DenseLayer(l_max, num_units=self.num_filters3, nonlinearity=rectify)
         # Softmax Output
         l_out = DenseLayer(l_dense, num_units=2, nonlinearity=softmax)
         self.model = l_out
 
     #Produce train/val data from input images
     def set_data(self, images, gts):
-        self.X_train, self.y_train = ([],[])
-        self.X_val, self.y_val = ([],[])
+        X_train, y_train = ([],[])
+        X_val, y_val = ([],[])
+        pad = self.pad
         for n in range(len(images)):
             image, gt = (images[n], gts[n])
             h, w = gts[n].shape
             for i in range(h):
                 for j in range(w):
-                    im_x, im_y = (i+PAD, j+PAD)
-                    block = image[:, im_x-PAD:im_x+PAD + 1, im_y-PAD:im_y+PAD + 1]
-                    if n < TRAIN_SIZE:
-                        self.X_train.append(block)
-                        self.y_train.append(gt[i,j])
-                    elif n >= len(images)-VAL_SIZE:
-                        self.X_val.append(block)
-                        self.y_val.append(gt[i,j])
-        self.X_train = np.array(self.X_train).astype(np.float32)
-        self.y_train = np.array(self.y_train).astype(np.int32)
-        self.X_val = np.array(self.X_val).astype(np.float32)
-        self.y_val = np.array(self.y_val).astype(np.int32)
-        print(len(self.X_train), len(self.y_train), len(self.X_val), len(self.y_val))
+                    im_x, im_y = (i+pad, j+pad)
+                    block = image[:, im_x-pad:im_x+pad + 1, im_y-pad:im_y+pad + 1]
+                    if n < self.train_size:
+                        X_train.append(block)
+                        y_train.append(gt[i,j])
+                    elif n >= len(images) - self.val_size:
+                        X_val.append(block)
+                        y_val.append(gt[i,j])
+        self.X_train = np.array(X_train).astype(np.float32)
+        self.y_train = np.array(y_train).astype(np.int32)
+        self.X_val = np.array(X_val).astype(np.float32)
+        self.y_val = np.array(y_val).astype(np.int32)
+        print(len(X_train), len(y_train), len(X_val), len(y_val))
 
     # Set a loss expression for training
     def set_train_loss(self):
@@ -143,7 +120,7 @@ class TinyNet:
         self.lr_schedule = {
             #0: 0.01,
             0: 0.001,
-            NUM_EPOCHS-5: 0.0001
+            self.num_epochs-5: 0.0001
         }
         self.lr = theano.shared(np.float32(self.lr_schedule[0]))
         self.updates = nesterov_momentum(self.train_loss, params,
@@ -155,7 +132,7 @@ class TinyNet:
                                    self.train_loss, updates=self.updates)
         val_fn = theano.function([self.input_var, self.target_var],
                                  [self.test_loss, self.test_acc])
-        for epoch in range(NUM_EPOCHS):
+        for epoch in range(self.num_epochs):
             if epoch in self.lr_schedule:
                 lr = np.float32(self.lr_schedule[epoch])
                 print(" setting learning rate to %.7f" % lr)
@@ -164,7 +141,7 @@ class TinyNet:
             train_batches = 0
             start_time = time.time()
             for batch in iterate_minibatches(self.X_train, self.y_train,
-                                             BATCH_SIZE, shuffle=True):
+                                             self.batch_size, shuffle=True):
                 inputs, targets = batch
                 train_err += train_fn(inputs, targets)
                 train_batches += 1
@@ -173,22 +150,35 @@ class TinyNet:
             val_acc = 0
             val_batches = 0
             for batch in iterate_minibatches(self.X_val, self.y_val,
-                                             BATCH_SIZE, shuffle=False):
+                                             self.batch_size, shuffle=False):
                 inputs, targets = batch
                 err, acc = val_fn(inputs, targets)
                 val_err += err
                 val_acc += acc
                 val_batches += 1
 
-            print("Epoch {} of {} took {:.3f}s".format(epoch + 1, NUM_EPOCHS,
+            print("Epoch {} of {} took {:.3f}s".format(epoch + 1, self.num_epochs,
                                                     time.time() - start_time))
             print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
             print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
             print("  validation accuracy:\t\t{:.2f} %".format(val_acc / val_batches * 100))
 
+    def print_params():
+        print
+        print ('='*50)
+        print ('PAD = %d' % self.pad)
+        print ('INPUT_SHAPE = ', self.input_shape)
+        print ('TRAIN_SIZE = %d' % self.train_size)
+        print ('NUM_FILTERS1 = %d' % self.num_filters1)
+        print ('NUM_FILTERS2 = %d' % self.num_filters2)
+        print ('NUM_FILTERS3 = %d' % self.num_filters3)
+        print ('NUM_EPOCHS = %d' % self.num_epochs)
+        print ('BATCH_SIZE = %d' % self.batch_size)
+        print ('='*50)
+        print
 
 # Extend borders for extriving blocks for every pixel
-def pad_images(images):
+def pad_images(images, PAD):
     new_images = []
     for image in images:
         h, w, c = image.shape
@@ -201,9 +191,7 @@ def pad_images(images):
 # WORKER
 def train_unary_model(images, gts):
     for pad in range(3,7,1):
-        PAD = pad
-        INPUT_SHAPE = (None, 3, 2*PAD + 1, 2*PAD + 1)
-        images = pad_images(images)
+        images = pad_images(images, pad)
         # From TF to TH order
         images = images.transpose(0,3,1,2)
         for train_size in range(5, MAX_TRAIN_SIZE, 5):
@@ -219,8 +207,9 @@ def train_unary_model(images, gts):
                             for batch_size in BATCH_SIZE_SET:
                                 BATCH_SIZE = batch_size
                                 # TRAIN
-                                print_params()
-                                model = TinyNet()
+                                model = TinyNet(PAD, TRAIN_SIZE,NUM_EPOCHS, NUM_FILTERS1,
+                                                NUM_FILTERS2, NUM_FILTERS3, BATCH_SIZE)
+                                model.print_params()
                                 model.set_data(images, gts)
                                 model.set_train_loss()
                                 model.set_test_loss()
@@ -245,3 +234,32 @@ def train_unary_model(images, gts):
 
 def segmentation(unary_model, images):
     return [np.zeros(img.shape[:2]) for img in images]
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    # Bottleneck
+    def residual_block(layer, num_filters):
+    l_conv1 = Conv2DLayer(layer, num_filters/4, filter_size=1, pad='same',
+    nonlinearity=rectify, W=HeNormal())
+    l_conv1 = batch_norm(l_conv1)
+
+    l_conv2 = Conv2DLayer(l_conv1, num_filters/4, filter_size=3, pad='same',
+    nonlinearity=rectify, W=HeNormal())
+    l_conv2 = batch_norm(l_conv2)
+
+    l_conv3 = Conv2DLayer(l_conv2, num_filters, filter_size=1, pad='same',
+    nonlinearity=rectify, W=HeNormal())
+    l_conv3 = batch_norm(l_conv3)
+    return ElemwiseSumLayer([l_conv3, layer])
+'''
