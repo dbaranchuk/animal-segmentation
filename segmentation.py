b7b90479d9c6d58b843c6ee31ad8725b71b296f3
Dmitry <dmitrybaranchuk@gmail.com>
2017-02-08 00:11:47 +0300

    ...

--- segmentation.py
+++ segmentation.py
@@ -20,15 +20,15 @@ from lasagne.updates import nesterov_momentum
 MAX_NUM_EPOCHS = 61
 
 # num filters sets for every layers
-NUM_FILTERS1_SET = (16, 16)
-NUM_FILTERS2_SET = (16, 32)
-NUM_FILTERS3_SET = (128, 256)
+NUM_FILTERS1 = 16
+NUM_FILTERS2 = 32
+NUM_FILTERS3 = 256
 
 PAD = 5
-BATCH_SIZE = 16384 #(8192, 12288, 16384)
+BATCH_SIZE = 32768 #16384 #(8192, 12288, 16384)
 TRAIN_SIZE = 45
 VAL_SIZE = 5
-NUM_EPOCHS = 50
+NUM_EPOCHS = 100
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     assert len(inputs) == len(targets)
@@ -89,12 +89,6 @@ def get_data(image, gt):
 # Net for unary terms
 class TinyNet:
     def __init__(self):
-        return
-    # init params
-    def set_params(self, NUM_FILTERS1, NUM_FILTERS2, NUM_FILTERS3):
-        self.num_filters1 = NUM_FILTERS1
-        self.num_filters2 = NUM_FILTERS2
-        self.num_filters3 = NUM_FILTERS3
         self.input_shape = (None, 3, 2*PAD + 1, 2*PAD + 1)
         self.input_var = T.tensor4('inputs')
         self.target_var = T.ivector('targets')
@@ -104,17 +98,17 @@ class TinyNet:
         # Input Layer
         l_in = InputLayer(self.input_shape, input_var=self.input_var)
         # Conv1
-        l_conv1 = Conv2DLayer(l_in, num_filters=self.num_filters1, filter_size=3,
+        l_conv1 = Conv2DLayer(l_in, num_filters=NUM_FILTERS1, filter_size=3,
                               nonlinearity=rectify, W=HeNormal())
         l_conv1 = batch_norm(l_conv1)
         # Conv2
-        l_conv2 = Conv2DLayer(l_conv1, num_filters=self.num_filters2, filter_size=2,
+        l_conv2 = Conv2DLayer(l_conv1, num_filters=NUM_FILTERS2, filter_size=2,
                               stride=2, nonlinearity=rectify, W=HeNormal())
         l_conv2 = batch_norm(l_conv2)
         # Pool
         l_max = MaxPool2DLayer(l_conv2, pool_size=(2, 2))
         # FC
-        l_dense = DenseLayer(l_max, num_units=self.num_filters3, nonlinearity=rectify)
+        l_dense = DenseLayer(l_max, num_units=NUM_FILTERS3, nonlinearity=rectify)
         # Softmax Output
         l_out = DenseLayer(l_dense, num_units=2, nonlinearity=softmax)
         self.model = l_out
@@ -157,7 +151,7 @@ class TinyNet:
         params = get_all_params(self.model, trainable=True)
         self.lr_schedule = {
             0: 0.01,
-            20: 0.001,
+            40: 0.001,
             #NUM_EPOCHS-5: 0.0001
         }
         self.lr = theano.shared(np.float32(self.lr_schedule[0]))
@@ -211,17 +205,6 @@ class TinyNet:
             preds.append(test_fn(block))
         print preds
 
-    def print_params(self):
-        print
-        print ('='*50)
-        print ('PAD = %d' % PAD)
-        print ('INPUT_SHAPE = ', self.input_shape)
-        print ('NUM_FILTERS1 = %d' % self.num_filters1)
-        print ('NUM_FILTERS2 = %d' % self.num_filters2)
-        print ('NUM_FILTERS3 = %d' % self.num_filters3)
-        print ('NUM_EPOCHS = %d' % NUM_EPOCHS)
-        print ('='*50)
-        print
 
 # Extend borders for extriving blocks for every pixel
 def pad_images(images, pad):
@@ -234,40 +217,22 @@ def pad_images(images, pad):
         new_images.append(new_image)
     return np.array(new_images)
 
-# WORKER
+# Main training function
 def train_unary_model(images, gts):
     images = pad_images(images, PAD)
+    # From TF to TH order
     images = images.transpose(0,3,1,2)
+
     model = TinyNet()
     model.set_data(images, gts)
+    model.build_cnn()
+    model.set_train_loss()
+    model.set_val_loss()
+    model.set_update()
+    model.train()
 
-    for ind in range(2):
-        num_filters1 = NUM_FILTERS1_SET[ind]
-        num_filters2 = NUM_FILTERS2_SET[ind]
-        for num_filters3 in NUM_FILTERS3_SET:
-            # TRAIN
-            model.set_params(num_filters1, num_filters2, num_filters3)
-            model.print_params()
-            model.build_cnn()
-            model.set_train_loss()
-            model.set_val_loss()
-            model.set_update()
-            model.train()
+    model.get_predictions(images[-1])
     return {}
-
-# Main training function
-#def train_unary_model(images, gts):
-#    images = pad_images(images)
-    # From TF to TH order
-#    images = images.transpose(0,3,1,2)
-
-#    model = TinyNet()
-#    model.set_data(images, gts)
-#    model.set_train_loss()
-#    model.set_test_loss()
-#    model.set_update()
-#    model.train()
-#    return {}
 
 def compute_weights(X, Y):
     A, B, sigma = (1., 1., 1.)
